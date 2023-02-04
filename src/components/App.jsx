9f689760b7148072593097e0fb5e112e5d94bbba
Pasha2907 <pavliksischuk2001@gmail.com>
2023-02-04 16:35:48 +0200

    Completed hw 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,6 +51,20 @@ export class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     return (
